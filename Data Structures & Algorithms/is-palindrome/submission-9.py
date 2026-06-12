ca2a65632a5ee7a985acc55a5f6b905fd6e194fe
s-aniruddh-nitrr <s.aniruddh.nitrr@gmail.com>
2026-06-12 16:41:15 +0530

    Add: is-palindrome - submission-9

---
+++ Data Structures & Algorithms/is-palindrome/submission-9.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+        for char in s:
+            if char != " " and char.isalnum():
+                res = res + char.lower()
+        print("res = " + res)
+        start = 0
+        end = len(res)-1
+        while(start < end):
+            if(res[start] != res[end]):
+                return False 
+            start+=1
+            end-=1
+        return True        
